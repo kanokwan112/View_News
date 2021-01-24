04b48c2c9adf06a788b3edc1121a6b934f520994
kanokwan112509 <sutanukanokwan@gmail.com>
2021-01-24 22:39:06 +0700

    View News

---
+++ lib/profile.dart
@@ -0,0 +1,234 @@
+import 'package:flutter/material.dart';
+import 'package:ViewNews/Screens/Welcome/welcome_screen.dart';
+import 'package:ViewNews/constants.dart';
+import 'package:ViewNews/drawer_menu.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyProfile extends StatelessWidget {
+  MyProfile({Key key}) : super(key: key);
+
+  var sizeBox = 14.0;
+
+  TextStyle bulletStyle = GoogleFonts.prompt(
+    fontSize: 28,
+    fontWeight: FontWeight.bold,
+  );
+
+  TextStyle contentBulletStyle = GoogleFonts.prompt(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+
+  TextStyle contentStyle = GoogleFonts.kanit(
+    fontSize: 18,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'My Profile',
+          style: contentStyle,
+        ),
+        automaticallyImplyLeading: true,
+      ),
+      drawer: DrawerMenu(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+              child: Column(
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Center(
+                      child: Text(
+                        "นางสาวกนกวรรณ  สุดอนุ\n    kanokwan sutanu",
+                        style: bulletStyle,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(30.0),
+                    child: Image.asset(
+                      "assets/images/01.jpg",
+                      height: 180.0,
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Align(
+                      child: new Text(
+                        'สาขาวิชาวิทยาการคอมพิวเตอร์\nคณะศิลปศาสตร์และวิทยาศาสตร์',
+                        style: contentStyle,
+                      ),
+                      alignment: Alignment(-1, 0),
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "ประวัติการศึกษา",
+                      style: contentBulletStyle,
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.arrow_forward),
+                    title: Align(
+                      child: new Text(
+                        'จบชั้นประถมศึกษาชั้นป.1 - 6 โรงเรียนบ้านหนองบักโทน',
+                        style: contentStyle,
+                      ),
+                      alignment: Alignment(-1.8, 0),
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.arrow_forward),
+                    title: Align(
+                      child: new Text(
+                        'จบชั้นมัธยมศึกษาปีที่ 1 - 6 โรงเรียนโคกสะอาดวิทยาคม',
+                        style: contentStyle,
+                      ),
+                      alignment: Alignment(-1.8, 0),
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "สำหรับติดต่อ",
+                      style: contentBulletStyle,
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.phone_android),
+                    title: Align(
+                      child: new Text(
+                        '088-1175-929',
+                        style: contentStyle,
+                      ),
+                      alignment: Alignment(-1, 0),
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Align(
+                      child: new Text(
+                        'FB:Lego Chip',
+                        style: contentStyle,
+                      ),
+                      alignment: Alignment(-1, 0),
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.email),
+                    title: Align(
+                      child: new Text(
+                        'kanokwan.suta61@sskru.ac.th',
+                        style: contentStyle,
+                      ),
+                      alignment: Alignment(-1, 0),
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "ประวัติการทำงาน",
+                      style: contentBulletStyle,
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.arrow_forward),
+                    title: Align(
+                      child: new Text(
+                        'เป็นนักศึกษาคณะศิลปศาสตร์และวิทยาศาสตร์,\n\nมหาวิทยาลัยราชภัฏศรีสะเกษ',
+                        style: contentStyle,
+                      ),
+                      alignment: Alignment(-1.8, 0),
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "ความสามารถพิเศษ",
+                      style: contentBulletStyle,
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ListTile(
+                    leading: Icon(Icons.developer_board),
+                    title: Align(
+                      child: new Text(
+                        'เชี่ยวชาญการอ่านในใจ',
+                        style: contentStyle,
+                      ),
+                      alignment: Alignment(-1, 0),
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ListTile(
+                    leading: Icon(Icons.developer_board),
+                    title: Align(
+                      child: new Text(
+                        'เชี่ยวชาญการทำอาหาร',
+                        style: contentStyle,
+                      ),
+                      alignment: Alignment(-1, 0),
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ListTile(
+                    leading: Icon(Icons.developer_board),
+                    title: Align(
+                      child: new Text(
+                        'เชี่ยวชาญการเขียนโปรแกรมด้วย Python',
+                        style: contentStyle,
+                      ),
+                      alignment: Alignment(-1, 0),
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ListTile(
+                    leading: Icon(Icons.developer_board),
+                    title: Align(
+                      child: new Text(
+                        'การพัฒนาโมบายแอพพลิเคชันด้วย Flutter',
+                        style: contentStyle,
+                      ),
+                      alignment: Alignment(-1, 0),
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "คติประจำใจ",
+                      style: contentBulletStyle,
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.arrow_forward),
+                    title: Align(
+                      child: new Text(
+                        '    อย่าคิดว่าทำไม่ได้ ถ้ายังไม่ได้ทำ',
+                        style: contentStyle,
+                      ),
+                      alignment: Alignment(-1.8, 0),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
